6adf332bf9ae6897c3de84e397939f8c3a985eb6
darumcat <alexs14@list.ru>
2025-04-03 16:17:03 +1100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -10,182 +10,85 @@ export const CONFIG = {
         }
     },
     TOKENS: {
-        FDRMCT: {
-            address: "0x5a631147bE09F4af9f4f1E817e304D12bDD6Eb22",
+        CRPTHZ: {
+            address: "0x9757112F515f6c3c8dCe912b595667780F67B3E8",
             abi: [
                 {
                     "inputs": [
-                        {
-                            "internalType": "address",
-                            "name": "account",
-                            "type": "address"
-                        }
+                        {"internalType": "address", "name": "account", "type": "address"}
                     ],
                     "name": "balanceOf",
                     "outputs": [
-                        {
-                            "internalType": "uint256",
-                            "name": "",
-                            "type": "uint256"
-                        }
+                        {"internalType": "uint256", "name": "", "type": "uint256"}
                     ],
                     "stateMutability": "view",
                     "type": "function"
                 },
                 {
                     "inputs": [
-                        {
-                            "internalType": "address",
-                            "name": "recipient",
-                            "type": "address"
-                        },
-                        {
-                            "internalType": "uint256",
-                            "name": "amount",
-                            "type": "uint256"
-                        }
+                        {"internalType": "address", "name": "recipient", "type": "address"},
+                        {"internalType": "uint256", "name": "amount", "type": "uint256"}
                     ],
                     "name": "transfer",
                     "outputs": [
-                        {
-                            "internalType": "bool",
-                            "name": "",
-                            "type": "bool"
-                        }
+                        {"internalType": "bool", "name": "", "type": "bool"}
                     ],
                     "stateMutability": "nonpayable",
                     "type": "function"
                 },
                 {
                     "inputs": [
-                        {
-                            "internalType": "address",
-                            "name": "sender",
-                            "type": "address"
-                        },
-                        {
-                            "internalType": "address",
-                            "name": "recipient",
-                            "type": "address"
-                        },
-                        {
-                            "internalType": "uint256",
-                            "name": "amount",
-                            "type": "uint256"
-                        }
+                        {"internalType": "address", "name": "sender", "type": "address"},
+                        {"internalType": "address", "name": "recipient", "type": "address"},
+                        {"internalType": "uint256", "name": "amount", "type": "uint256"}
                     ],
                     "name": "transferFrom",
                     "outputs": [
-                        {
-                            "internalType": "bool",
-                            "name": "",
-                            "type": "bool"
-                        }
+                        {"internalType": "bool", "name": "", "type": "bool"}
                     ],
-                    "stateMutability": "nonpayable",
-                    "type": "function"
-                },
-                {
-                    "inputs": [
-                        {
-                            "internalType": "uint256",
-                            "name": "amount",
-                            "type": "uint256"
-                        }
-                    ],
-                    "name": "mint",
-                    "outputs": [],
                     "stateMutability": "nonpayable",
                     "type": "function"
                 }
             ],
             decimals: 18
         },
-        CRPTHZ: {
-            address: "0x9757112F515f6c3c8dCe912b595667780F67B3E8",
+        FDRMCT: {
+            address: "0x5a631147bE09F4af9f4f1E817e304D12bDD6Eb22",
             abi: [
+                // Такой же ABI как у CRPTHZ
                 {
                     "inputs": [
-                        {
-                            "internalType": "address",
-                            "name": "account",
-                            "type": "address"
-                        }
+                        {"internalType": "address", "name": "account", "type": "address"}
                     ],
                     "name": "balanceOf",
                     "outputs": [
-                        {
-                            "internalType": "uint256",
-                            "name": "",
-                            "type": "uint256"
-                        }
+                        {"internalType": "uint256", "name": "", "type": "uint256"}
                     ],
                     "stateMutability": "view",
                     "type": "function"
                 },
                 {
                     "inputs": [
-                        {
-                            "internalType": "address",
-                            "name": "recipient",
-                            "type": "address"
-                        },
-                        {
-                            "internalType": "uint256",
-                            "name": "amount",
-                            "type": "uint256"
-                        }
+                        {"internalType": "address", "name": "recipient", "type": "address"},
+                        {"internalType": "uint256", "name": "amount", "type": "uint256"}
                     ],
                     "name": "transfer",
                     "outputs": [
-                        {
-                            "internalType": "bool",
-                            "name": "",
-                            "type": "bool"
-                        }
+                        {"internalType": "bool", "name": "", "type": "bool"}
                     ],
                     "stateMutability": "nonpayable",
                     "type": "function"
                 },
                 {
                     "inputs": [
-                        {
-                            "internalType": "address",
-                            "name": "sender",
-                            "type": "address"
-                        },
-                        {
-                            "internalType": "address",
-                            "name": "recipient",
-                            "type": "address"
-                        },
-                        {
-                            "internalType": "uint256",
-                            "name": "amount",
-                            "type": "uint256"
-                        }
+                        {"internalType": "address", "name": "sender", "type": "address"},
+                        {"internalType": "address", "name": "recipient", "type": "address"},
+                        {"internalType": "uint256", "name": "amount", "type": "uint256"}
                     ],
                     "name": "transferFrom",
                     "outputs": [
-                        {
-                            "internalType": "bool",
-                            "name": "",
-                            "type": "bool"
-                        }
+                        {"internalType": "bool", "name": "", "type": "bool"}
                     ],
-                    "stateMutability": "nonpayable",
-                    "type": "function"
-                },
-                {
-                    "inputs": [
-                        {
-                            "internalType": "uint256",
-                            "name": "amount",
-                            "type": "uint256"
-                        }
-                    ],
-                    "name": "mint",
-                    "outputs": [],
                     "stateMutability": "nonpayable",
                     "type": "function"
                 }
@@ -205,11 +108,7 @@ export const CONFIG = {
                 "inputs": [],
                 "name": "feePercent",
                 "outputs": [
-                    {
-                        "internalType": "uint256",
-                        "name": "",
-                        "type": "uint256"
-                    }
+                    {"internalType": "uint256", "name": "", "type": "uint256"}
                 ],
                 "stateMutability": "view",
                 "type": "function"
@@ -218,22 +117,14 @@ export const CONFIG = {
                 "inputs": [],
                 "name": "owner",
                 "outputs": [
-                    {
-                        "internalType": "address",
-                        "name": "",
-                        "type": "address"
-                    }
+                    {"internalType": "address", "name": "", "type": "address"}
                 ],
                 "stateMutability": "view",
                 "type": "function"
             },
             {
                 "inputs": [
-                    {
-                        "internalType": "uint256",
-                        "name": "_feePercent",
-                        "type": "uint256"
-                    }
+                    {"internalType": "uint256", "name": "_feePercent", "type": "uint256"}
                 ],
                 "name": "setFee",
                 "outputs": [],
@@ -242,34 +133,14 @@ export const CONFIG = {
             },
             {
                 "inputs": [
-                    {
-                        "internalType": "address",
-                        "name": "tokenIn",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "address",
-                        "name": "tokenOut",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "amountIn",
-                        "type": "uint256"
-                    },
-                    {
-                        "internalType": "address",
-                        "name": "recipient",
-                        "type": "address"
-                    }
+                    {"internalType": "address", "name": "tokenIn", "type": "address"},
+                    {"internalType": "address", "name": "tokenOut", "type": "address"},
+                    {"internalType": "uint256", "name": "amountIn", "type": "uint256"},
+                    {"internalType": "address", "name": "recipient", "type": "address"}
                 ],
                 "name": "swap",
                 "outputs": [
-                    {
-                        "internalType": "uint256",
-                        "name": "amountOut",
-                        "type": "uint256"
-                    }
+                    {"internalType": "uint256", "name": "amountOut", "type": "uint256"}
                 ],
                 "stateMutability": "nonpayable",
                 "type": "function"
