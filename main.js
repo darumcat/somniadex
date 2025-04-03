f5c7fc47b993a16b3d2c575aa1d30de0bc9cd147
darumcat <alexs14@list.ru>
2025-04-03 13:08:25 +1100

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-import { connectWallet, checkNetwork } from './wallet.js';
-import { updateUI } from './ui.js';
-
-// Инициализация
-window.onload = () => {
-    connectWallet();
-    checkNetwork();
-};
