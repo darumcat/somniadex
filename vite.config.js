cf9c6eda43fec6dce789722300e6270cdff66791
darumcat <alexs14@list.ru>
2025-04-03 12:41:05 +1100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,7 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
-  publicDir: 'public',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      },
-      external: [] // Явно указываем внешние зависимости, если нужно
-    }
-  },
   server: {
     open: true,
-    port: 3000
   },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src') // Добавляем алиас для удобных путей
-    }
-  }
-})
+});
